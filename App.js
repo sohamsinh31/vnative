ee34087fd7cb5f9c9cd0a2edeab3a6936ce90eef
sohamsinh31 <sohamb2019@gmail.com>
2022-08-13 22:29:03 +0530

    2.0

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Image, Text, View } from 'react-native';
+import { useEffect,useState } from 'react';
+import UserAvatar from 'react-native-user-avatar';
 
+// import { Avatar } from "@rneui/themed";
 export default function App() {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+ async function fetchh(){
+    fetch('http://192.168.0.101:8000/my/',{
+      method:"GET"
+    }).then(resp => resp.json()).then(data => {
+      setdata(data)
+    })
+  }
+  fetchh();
+  }, [])
+  //console.log(data.id)
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        color:'red',
+        fontSize:'30px',
+        fontWeight:'bold'
+      }}>VPLAY</Text>
+      <br></br>
+      { 
+      data.map(value => {
+        //console.log("http://191.168.0.101:8000/"+value.picture)
+        return ( 
+      <UserAvatar
+      size={45}
+      style={{
+        backgroundColor:'transperent',
+        alignItems:'left'
+      }}
+      src={
+      'http://192.168.0.101:8000/'+value.picture
+      }
+      />
+      )})
+  }
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0e0e0e',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
